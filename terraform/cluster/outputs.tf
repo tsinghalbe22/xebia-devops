e1a470ca60e0667faab3cde82e2fcbe44ef2a503
tsinghalbe22 <tsinghal_be22@thapar.edu>
2025-07-09 22:04:54 +0530

    Update outputs.tf

--- terraform/cluster/outputs.tf
+++ terraform/cluster/outputs.tf
@@ -1,18 +1,25 @@
 output "acr_url" {
-  value = azurerm_container_registry.acr.login_server
-}
-
-output "aks_api_server" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config.0.host
-  sensitive = true
-}
-
-output "aks_client_id" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
-  sensitive = true
+  value       = azurerm_container_registry.acr.login_server
+  description = "The URL of the Azure Container Registry"
 }
 
 output "acr_name" {
-  value = azurerm_container_registry.acr.name
+  value       = azurerm_container_registry.acr.name
   description = "The name of the Azure Container Registry"
+}
+
+output "aks_api_server" {
+  value       = azurerm_kubernetes_cluster.aks.kube_config.0.host
+  description = "The API server URL of the AKS cluster"
+  sensitive   = true
+}
+
+output "aks_cluster_name" {
+  value       = azurerm_kubernetes_cluster.aks.name
+  description = "The name of the AKS cluster"
+}
+
+output "resource_group_name" {
+  value       = azurerm_resource_group.rg.name
+  description = "The name of the resource group"
 }
