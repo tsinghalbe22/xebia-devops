0729dfc9d20ae50307d052834b75eaf299826836
tsinghalbe22 <tsinghal_be22@thapar.edu>
2025-07-09 21:20:31 +0530

    Update main.tf

--- terraform/cluster/main.tf
+++ terraform/cluster/main.tf
@@ -57,16 +57,3 @@ resource "azurerm_subnet" "subnet" {
 }
 
 # Output the required information
-output "acr_url" {
-  value = azurerm_container_registry.acr.login_server
-}
-
-output "aks_api_server" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config.0.host
-  sensitive = true
-}
-
-output "aks_client_id" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
-  sensitive = true
-}
